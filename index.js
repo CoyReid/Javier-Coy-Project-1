7f2834bbfc0dccd5ec9840373ba9092a97e2d6c8
Javier Chireno <javierchireno04@gmail.com>
2021-08-03 12:59:39 -0400

    Added button functions and Eventlisntener

--- index.js
+++ index.js
@@ -12,30 +12,17 @@
 //     .then(char => renderSecondChar(char))
 // }
 
-const marvelUrl = ""
-const dcUrl = ""
-const movieUrl = ""
-const animeUrl = ""
-const videoGameUrl = ""
+const allUrl = "http://localhost:3000/"
 
-const getMarvel = () => {
-
+const getAll = (directory, min, max) => {
+    fetch(`${allUrl}${directory}/${randomId(min, max)}`)
+    .then(resp => resp.json())
+    .then(char => renderFirstChar(char))
 }
-
-const getDc = () => {
-
-}
-
-const getVideoGame = () => {
-
-}
-
-const getMovie = () => {
-
-}
-
-const getAnime = () => {
-
+const getAllTwo = (directory, min, max) => {
+    fetch(`${allUrl}${directory}/${randomId(min, max)}`)
+    .then(resp => resp.json())
+    .then(char => renderSecondChar(char))
 }
 
 const randomId = (min, max) => {
@@ -47,56 +34,122 @@ const randomId = (min, max) => {
 
 const renderFirstChar = (char) => {
     const imageOne = finder("#char-one-img");
-    imageOne.src = char.image.url;
+    imageOne.src = char.img;
 
     const nameOne = finder("#char-one-name");
     nameOne.innerText = char.name;
+    
+    const votesOne = finder("#char-one-votes")
+    votesOne.innerText = `Votes: ${char.votes}`;
 
-    const raceOne = finder("#char-one-race");
-    raceOne.innerText = `Race: ${char.appearance.race}`;
+    const originOne = finder("#char-one-origin")
+    originOne.innerText = char.origin;
+
     
-    const genderOne = finder("#char-one-gender");
-    genderOne.innerText = `Gender: ${char.appearance.gender}`;
-    
-    const heightOne = finder("#char-one-height");
-    heightOne.innerText = `Height: ${char.appearance.height[0]}`;
-    
-    const weightOne = finder("#char-one-weight");
-    weightOne.innerText = `Weight: ${char.appearance.weight[0]}`;
-    
-    const originOne = finder("#char-one-publisher");
-    originOne.innerText = `Origin: ${char.biography.publisher}`;
 }
 
 const renderSecondChar = (char) => {
     const imageTwo = finder("#char-two-img");
-    imageTwo.src = char.image.url;
+    imageTwo.src = char.img;
 
     const nameTwo = finder("#char-two-name");
     nameTwo.innerText = char.name;
 
-    const raceTwo = finder("#char-two-race");
-    raceTwo.innerText = `Race: ${char.appearance.race}`;
-    
-    const genderTwo = finder("#char-two-gender");
-    genderTwo.innerText = `Gender: ${char.appearance.gender}`;
-    
-    const heightTwo = finder("#char-two-height");
-    heightTwo.innerText = `Height: ${char.appearance.height[0]}`;
-    
-    const weightTwo = finder("#char-two-weight");
-    weightTwo.innerText = `Weight: ${char.appearance.weight[0]}`;
-    
-    const originTwo = finder("#char-two-publisher");
-    originTwo.innerText = `Origin: ${char.biography.publisher}`;
+    const votesTwo = finder("#char-two-votes")
+    votesTwo.innerText = `Votes: ${char.votes}`;
+
+    const originTwo = finder("#char-two-origin")
+    originTwo.innerText = char.origin;
 }
 
 const init = () => {
     firstCardData();
     secondCardData();
+    
 }
 
 
 const finder = (type) => {
     return document.querySelector(type)
+}
+
+const btnEvents = () => {
+    const marvelButton = finder("#marvel");
+    marvelButton.addEventListener("click", e => getMarvel())
+    
+    const dcButton = finder("#dc");
+    dcButton.addEventListener("click", e => getDC())
+    
+    const animeButton = finder("#anime");
+    animeButton.addEventListener("click", e => getAnime())
+
+    const moviesButton = finder("#movie");
+    moviesButton.addEventListener("click", e => getMovies())
+
+    const vgButton = finder("#video-game");
+    vgButton.addEventListener("click", e => getVG())
+
+    const marvelButtonTwo = finder("#marvel-two");
+    marvelButtonTwo.addEventListener("click", e => getMarvelTwo())
+
+    const dcButtonTwo = finder("#dc-two");
+    dcButtonTwo.addEventListener("click", e => getDCTwo())
+
+    const animeButtonTwo = finder("#anime-two");
+    animeButtonTwo.addEventListener("click", e => getAnimeTwo())
+
+    const moviesButtonTwo = finder("#movie-two");
+    moviesButtonTwo.addEventListener("click", e => getMoviesTwo())
+
+    const vgButtonTwo = finder("#video-game-two");
+    vgButtonTwo.addEventListener("click", e => getVGTwo())
+
+    const randomButton = finder("#random-button")
+    randomButton.addEventListener("click", e=> getRandom() )
+}
+
+btnEvents();
+const getMarvel = () => {
+     getAll("marvel","88","128")
+}
+
+const getDC = () => {
+    getAll("dc","129","144")
+}
+
+const getAnime = () => {
+    getAll("anime","145","159")
+}
+
+const getMovies = () => {
+    getAll("movies","1","37")
+}
+
+const getVG = () => {
+    getAll("videogames","38","87")
+}
+
+const getMarvelTwo = () => {
+    getAllTwo("marvel","88","128")
+}
+
+const getDCTwo = () => {
+   getAllTwo("dc","129","144")
+}
+
+const getAnimeTwo = () => {
+   getAllTwo("anime","145","159")
+}
+
+const getMoviesTwo = () => {
+   getAllTwo("movies","1","37")
+}
+
+const getVGTwo = () => {
+   getAllTwo("videogames","38","87")
+}
+
+const getRandom = () => {
+
+
 }
